61f3a0d9ff838743edb84906b0d61d2f157cc96e
DavidDgebuadze <75311974+DatoDgebuadze@users.noreply.github.com>
2024-04-15 22:59:00 -0400

    updated, models app.py for flask

--- Website/app.py
+++ Website/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
 import pandas as pd
 import sqlite3
+import os
+
+
+
+current_directory = os.getcwd()
+print("Current Directory:", current_directory)
+
 
 app = Flask(__name__)
 
@@ -10,7 +17,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('./website/templates/index.html', app_name='Our ML Group project')
+    return render_template('index.html', app_name='Our ML Group project')
 
 
 @app.route('/about')
@@ -20,7 +27,7 @@ def about():
 
 @app.route("/data")
 def data():
-    con = sqlite3.connect('./database/pd_data.db')
+    con = sqlite3.connect('../database/pd_data.db')
     # Query SQLite database to read data
     result_1 = pd.read_sql_query("SELECT * FROM pd_euros", con)
     # result_2 = pd.read_sql_query("SELECT * FROM pd_fuel", con)
